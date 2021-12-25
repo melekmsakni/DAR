24a04b3cf144b6085a9c42c3976f2196c2e11b31
melek msakni <65375259+melekmsakni@users.noreply.github.com>
2021-12-26 00:02:00 +0100

    Update MyInterface.java

--- src/tp5/MyInterface.java
+++ src/tp5/MyInterface.java
@@ -6,4 +6,10 @@ import java.rmi.RemoteException;
 public interface MyInterface extends Remote {
 
     String sayHello()throws RemoteException;
+    String majuscule(String x) throws RemoteException;
+    int position(String x,char c) throws RemoteException;
+    boolean palindrome(String x) throws RemoteException;
+    void deposit(double x) throws RemoteException;
+    void withdraw(double x) throws RemoteException;
+    void createAccount(int x,String y,double z) throws RemoteException;
 }
