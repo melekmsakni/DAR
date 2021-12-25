37123f9ae87b64e883675bbdf1b0dd10267c8a08
melek msakni <65375259+melekmsakni@users.noreply.github.com>
2021-12-26 00:00:39 +0100

    Update Server.java

--- src/tp5/Server.java
+++ src/tp5/Server.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Locale;
 
 public class Server extends UnicastRemoteObject implements MyInterface {
 
@@ -11,6 +12,50 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
     public String sayHello() {
         return "Hello World !!";
+    }
+
+    public String majuscule(String x){
+        return x.toUpperCase();
+    }
+    public int position(String x,char c)
+    {
+        return x.indexOf(c);
+    }
+
+
+    public boolean palindrome(String a)
+    {
+        int j=a.length()-1;
+        int i=0;
+        while(a.charAt(i)==a.charAt(j) && i<a.length()/2 && j>(a.length()/2)-1  ) {
+            i++;
+            j--;
+        }
+
+        if (i==a.length()/2)
+            return true;
+        else
+            return false;
+    }
+
+    BankAccount bc;
+
+
+    @Override
+    public void createAccount(int x, String y, double z) throws RemoteException {
+     bc=new BankAccount(x,y,z);
+     System.out.println(bc);
+    }
+
+    @Override
+    public void deposit(double x) throws RemoteException {
+        bc.deposit(x);
+        System.out.println(bc);
+    }
+    @Override
+    public void withdraw(double x) throws RemoteException {
+        bc.withdraw(x);
+        System.out.println(bc);
     }
 
     public static void main(String[] args) throws RemoteException, MalformedURLException {
