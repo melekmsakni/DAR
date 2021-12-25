237db418dcb15895825e1df2ad158ae1272b6a46
melek msakni <65375259+melekmsakni@users.noreply.github.com>
2021-12-26 00:03:15 +0100

    Update Client.java

--- src/tp5/Client.java
+++ src/tp5/Client.java
@@ -7,7 +7,13 @@ public class Client {
     public static void main(String[] args) {
         try {
             MyInterface service1 = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
-            System.out.println(service1.sayHello());
+            /*System.out.println(service1.majuscule("wadii"));
+            System.out.println(service1.position("wadii",'i'));
+            System.out.println(service1.palindrome("azza"));*/
+            service1.createAccount(1,"wadii",0);
+            service1.deposit(100);
+            service1.withdraw(50);
+
 
         } catch (Exception e) {
             System.out.println("Error");
